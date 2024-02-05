6bb3c9fb91b2bd99e383d8a5ab8f1b7a41fa542c
Kassam <kassam.housseny@gmail.com>
2024-02-05 11:25:30 +0100

    Use HashRouter instead as github-pages does not support well BrowerRouter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { Loader } from '../components/Loader';
 import { store } from '../redux/store';
 import { routes } from '../routes';
 
 export const App = () => {
-  const router = createBrowserRouter(routes);
+  const router = createHashRouter(routes);
 
   return (
     <Provider store={store}>
