a9dfb9413f9ac4d737a5649d8950de7d6e1cab90
Kassam <kassam.housseny@gmail.com>
2024-02-16 14:54:28 +0100

    Update base href and router in index.html and index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Loader } from '../components/Loader';
 import { persistor, store } from '../redux/store';
 import { routes } from '../routes';
 
 export const App = () => {
-  const router = createHashRouter(routes);
+  const router = createBrowserRouter(routes);
 
   return (
     <Provider store={store}>
